207c3ce5f002a96714e163a2917ba728bab186bf
Jorrie Brettin <jorriebrettin@gmail.com>
2016-02-14 15:59:32 -0500

    Randomly swimming fish

--- Boids.spritebuilder/Source/Boid.swift
+++ Boids.spritebuilder/Source/Boid.swift
@@ -9,7 +9,53 @@
 import Foundation
 
 class Boid: CCSprite {
+  
+  let VISIBLE_DIS : Float = 75
+  let CAUTION_DIS : Float = 10
+  
+  var velocity = CGPoint()
+  var delegate : BoidDelegate!
 
+  override func update(delta: CCTime) {
+    let v1 = rule1()
+    let v2 = rule2()
+    let v3 = rule3()
+    
+    velocity = sumOf([velocity,v1,v2,v3])
+    position = sumOf([velocity,position])
+    position = CGPoint(x: position.x % screenWidth , y: position.y % screenHeight)
+  }
   
+  private func sumOf(points:[CGPoint]) -> CGPoint{
+    var sum = CGPoint(x: 0, y: 0)
+    for point in points{
+      sum = CGPoint(x: sum.x + point.x, y: sum.y + point.y)
+    }
+    return sum
+  }
   
+  // Boids try to fly towards the center of neighbouring boids.
+  func rule1() -> CGPoint {
+    
+    return CGPoint()
+  }
+  
+  // Boids try to keep a small distance away from other boids.
+  func rule2() -> CGPoint {
+    return CGPoint()
+  }
+  
+  // Boids try to match velocity with nearby boids.
+  func rule3() -> CGPoint {
+    return CGPoint()
+  }
+  
+  func averageBoid(positions: [Boid]) -> CGPoint{
+    return CGPoint()
+  }
+  
+}
+
+protocol BoidDelegate {
+  func getBoidsWithinDistance(x:Float, ofBoid:Boid) -> [Boid]
 }
--- Boids.spritebuilder/Source/MainScene.swift
+++ Boids.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,65 @@
+//
+//  MainScene.swift
+//  MainScene
+//
+//  Created by Jottie Brerrin on 2/14/16.
+//  Copyright © 2016 Apportable. All rights reserved.
+//
+
 import Foundation
 
-class MainScene: CCNode {
+var screenWidth,screenHeight : CGFloat!
 
+class MainScene: CCNode {
+  let NUM_BOIDS : Int = 20;
+  let START_VELOCITY_MAGNITUDE : Double = 0.5
+  
+  override func onEnter() {
+    super.onEnter()
+    if UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.Pad {
+      let screenSize: CGRect = UIScreen.mainScreen().bounds
+      screenWidth = screenSize.width / 2
+      screenHeight = screenSize.height / 2
+    } else {
+      let screenSize: CGRect = UIScreen.mainScreen().bounds
+      screenWidth = screenSize.width / 2 * UIScreen.mainScreen().scale
+      screenHeight = screenSize.height / 2 * UIScreen.mainScreen().scale
+    }
+    
+    for (var i = 0; i < NUM_BOIDS; i++){
+      let boid = generateBoid()
+      addChild(boid)
+    }
+  }
+  
+  private func generateBoid() -> Boid {
+    let boid = CCBReader.load("Boid") as! Boid
+    boid.position = CGPoint(x: CGFloat(drand48()) * screenWidth, y: CGFloat(drand48()) * screenHeight)
+    boid.velocity = generateVelocity()
+    return boid
+  }
+  
+  private func generateVelocity() -> CGPoint {
+    let randX = drand48()
+    let correspondingY = sqrt(1 - randX*randX)
+    let vector = CGPoint(x: randX * START_VELOCITY_MAGNITUDE, y: correspondingY * START_VELOCITY_MAGNITUDE)
+    return vector
+  }
+  
+}
+
+extension MainScene : BoidDelegate {
+  
+  func getBoidsWithinDistance(x: Float, ofBoid: Boid) -> [Boid] {
+    let boids = [Boid]()
+    
+    //get boids within distance, including those on the other side of the screen
+    
+    return boids
+  }
+  
+  private func getDistanceBetween(boid1:Boid, boid2:Boid) -> Float{
+    return 0.0
+  }
+  
 }
