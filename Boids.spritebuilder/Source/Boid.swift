0a191da87da9b17423d3552dfcfcae6ddc81bf00
Jorrie Brettin <jorriebrettin@gmail.com>
2016-02-14 13:43:14 -0500

    Boid art and ccb

---
+++ Boids.spritebuilder/Source/Boid.swift
@@ -0,0 +1,15 @@
+//
+//  Boid.swift
+//  Boids
+//
+//  Created by Jottie Brerrin on 2/14/16.
+//  Copyright © 2016 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Boid: CCSprite {
+
+  
+  
+}
