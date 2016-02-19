b4bda1d79d0c78fd03d724aaca6e758f890d5469
Jorrie Brettin <jorriebrettin@gmail.com>
2016-02-19 14:58:31 -0500

    Updated tutorial

--- Boids.spritebuilder/Source/Boid.swift
+++ Boids.spritebuilder/Source/Boid.swift
@@ -10,38 +10,34 @@ import Foundation
 
 class Boid: CCSprite {
   
-  let VISIBLE_DIS : CGFloat = 60
-  let CAUTION_DIS : CGFloat = 20
   let MAX_VELOCITY : CGFloat = 1.5
-  
-  //vars for different boid behaviors
-  //lower number = higher effect
-  let COHESION : CGFloat = 900
-  let REPULSION : CGFloat = 150
-  let VEL_MATCHING : CGFloat = 600
   
   var velocity = CGPoint()
   var delegate : BoidDelegate!
 
   override func update(delta: CCTime) {
+    
     speedCheck()
-    position = sumOf([velocity,position])
+    position = sumOf([velocity,position]) //update position by velocity
     position = modulo(position)
     
     rotation = atan2(Float(velocity.x), Float(velocity.y)) * 180 / Float(M_PI)
   }
   
+  //Limits magnitude of boid velocity
   private func speedCheck(){
     let speed = sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
     velocity = speed < MAX_VELOCITY ? velocity : CGPoint(x: velocity.x / speed * MAX_VELOCITY, y: velocity.y / speed * MAX_VELOCITY)
   }
   
+  //Used to wrap boids around screen
   private func modulo(point:CGPoint) -> CGPoint{
     let newX = point.x < 0 ? screenWidth + (point.x % screenWidth) : point.x % screenWidth
     let newY = point.y < 0 ? screenHeight + (point.y % screenHeight) : point.y % screenHeight
     return CGPoint(x: newX, y: newY)
   }
   
+  //Returns the sum of an array of points
   private func sumOf(points:[CGPoint]) -> CGPoint{
     var sum = CGPoint(x: 0, y: 0)
     for point in points{
@@ -50,13 +46,6 @@ class Boid: CCSprite {
     return sum
   }
   
-  // Boids swim toward other nearby boids.
-
-  
-  // Boids keep a small distance from nearby boids.
-
-  
-  // Boids match velocity with nearby boids.
 
   
 }
